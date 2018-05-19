5795ee3c644c20884394a82c2989553fcd1a8105
Tedtanium <30739356+Tedtanium@users.noreply.github.com>
2018-05-18 17:57:58 -0700

    Update and rename nfs-auto-updater-for-ldap-users to nfsautoupdate.sh

--- machine-scripts/nfs/nfsautoupdate.sh
+++ machine-scripts/nfs/nfsautoupdate.sh
@@ -3,25 +3,33 @@
 #This should be run every minute as a cron job.
 
 #Greps for LDAP users, and stores their usernames in a file. 
+
+
 #This one overwrites the old ones, as otherwise the function would get drastically longer with each and every run of it.
 getent passwd | grep 500 | awk -F ":" '{print $1}' > ldap-users
 
-#Stores username as $uservariable.
-
 #Starts for loop.
+for line in $(cat ldap-users);
+
+do
+#Stores username as $uservariable.
+  uservariable=$line
 
 #Check to see if /home/$uservariable exists; if not then move onto the next section of the for loop (How do?)
-
+  if [ ! -e /home/$uservariable ]
+  then
 #Creates user directory in /home/$uservariable
-mkdir /home/$uservariable
+    mkdir /home/$uservariable
 
 #Echoes user directory into the /etc/exports file, where it will be shared via NFS. (UNFINISHED)
-echo "/home/$uservariable   *(rw,sync)" >> /etc/exports
+    echo "/home/$uservariable   *(rw,sync)" >> /etc/exports
 
 #Changes permissions of /home/$uservariable share.
-chmod /home/$uservariable 0700
+    chmod 0700 /home/$uservariable
 
 #Changes ownership of /home/$uservariable to the user.
-chown /home/$uservariable $uservariable
-
+    chown  $uservariable /home/$uservariable
+#End of the if check.
+  fi
 #End of for loop.
+done
